176d303c3d3f953db52a766a506af7b23c817bb9
cdaguanno <102310974+cdaguanno@users.noreply.github.com>
2025-11-25 12:22:07 -0500

    New home page ECS state tables

---
+++ app/ec2instance.py
@@ -0,0 +1,17 @@
+import http.client
+import json
+
+def GetInstanceState(arg1):
+    conn = http.client.HTTPSConnection("6yah8uooec.execute-api.us-east-2.amazonaws.com")
+    
+    #payload = json.dumps({...})
+    
+    
+    headers = {
+      'Content-Type': 'application/json'
+    }
+    conn.request("GET", "/prod/instances?region=" + arg1, headers=headers)
+    res = conn.getresponse()
+    data = res.read()
+    return json.loads(data.decode("utf-8"))
+    #print(data.decode("utf-8"))
--- app/views.py
+++ app/views.py
@@ -5,18 +5,38 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from app import ec2instance
 
 def home(request):
     """Renders the home page."""
-    assert isinstance(request, HttpRequest)
+# Get EC2 instance data
+    east1_data = ec2instance.GetInstanceState('us-east-1')
+    east2_data = ec2instance.GetInstanceState('us-east-2')
+    canada_Central1_data = ec2instance.GetInstanceState('ca-central-1')
+    
     return render(
         request,
         'app/index.html',
         {
-            'title':'Home Page',
-            'year':datetime.now().year,
+            'title': 'Home Page',
+            'year': datetime.now().year,
+            'regions': [
+                {
+                    'name': east1_data.get('region'),
+                    'instances': east1_data.get('instances', [])
+                },
+                {
+                    'name': east2_data.get('region'),
+                    'instances': east2_data.get('instances', [])
+                },
+                {
+                    'name': canada_Central1_data.get('region'),
+                    'instances': canada_Central1_data.get('instances', [])
+                }
+            ]
         }
     )
+
 
 def contact(request):
     """Renders the contact page."""
